7665d571401134b4c50fb55660a8b5f8aec807b2
agmorrow <morrow.austin5@gmail.com>
2022-07-11 15:23:11 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Services from './components/Services';
+import HowItWorks from './components/HowItWorks';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/howitworks' element={<HowItWorks/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+    </Routes>
+    <Footer />
+    </>
   );
 }
 

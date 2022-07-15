4c9882d4a8ca87e7898abe70c4db5be59d894444
agmorrow <morrow.austin5@gmail.com>
2022-07-14 19:48:23 -0600

    navbar almost responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import LifeChoicesLogo from '../assets/life_choices.png';
+import React, { useState } from 'react'
 import Button from './Button';
+import LifeChoicesLogo from '../assets/life_choices.png'
 
 const Navbar = () => {
-  let Links =[
-    {name:'HOME',link:'/'},
-    {name:'SERVICES',link:'/'},
-    {name:'HOW IT WORKS',link:'/'},
-    {name:'ABOUT',link:'/'},
-  ];
+    let Links =[
+      {name:"HOME",link:"/"},
+      {name:"SERVICE",link:"/"},
+      {name:"HOW IT WORKS",link:"/"},
+      {name:"ABOUT",link:"/"},
+    ];
+    let [open,setOpen]=useState(false);
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex bg-white py-4  flex items-center justify-between'>
-        <span className='px-20'>
-          <img src={LifeChoicesLogo} alt='Dragon Fly Logo' className='max-w-[250px]'></img>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+        <img src={LifeChoicesLogo} alt='Dragonfly logo' className='max-w-[200px] ml-20 pb-2'></img>
         </span>
-        <ul className='md:flex md:items-center pr-8'>
-          {
-            Links.map((link)=>(
-              <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-                <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
-              </li>
-            ))
-          }
-          <Button>
-            CONTACT
-          </Button>
-        </ul>
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
+
+      <ul className={`md:flex md:items-center pt-40 md:pt-0 md:pb-0 absolute pb-10 md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+        <Button>
+          CONTACT
+        </Button>
+      </ul>
       </div>
     </div>
   )

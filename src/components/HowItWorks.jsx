eff320bb349eb157611b83b6c5885c11d5b5ee56
agmorrow <morrow.austin5@gmail.com>
2022-07-27 15:37:45 -0600

    updated FAQ Section

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,16 +2,49 @@ import React from 'react'
 
 const HowItWorks = () => {
   return (
-    <div className="mx-20">
-      <h1>What is Life Coaching?</h1>
-      <p>Life coaching is a process of goals set by you. We all know each step has roadblocks. We discuss roadblocks and what options you have when they occur. Life coaching is a method to practice small goals towards that final goal. </p>
-      <h1>What life coaching is not.</h1>
-      <p>Life coaching is not counseling, that's for a licensed professional. If you are having mental health issues or immediate crisis consult a licensed therapist or call 911.</p>
-      <h1>How does life coaching work?</h1>
-      <p>We decide together on the best plan based on the results you are looking to accomplish. Hourly rates allow you to detemine how many visits you think you will need. Monthly rates include weekly meetings by phone, and email correspondence and require a three month commitment. 
+ 
 
+<div className="bg-lightblue py-20 px-4">
+    <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
+        <h2 className="mr-8 w-full md:w-1/3 text-3xl font-extrabold leading-9">
+            Frequently-asked questions
+        </h2>
+        <dl className="w-full md:w-2/3">
+            <dt className="mb-4">
+                <h3 className="text-xl font-semibold">
+                What is Life Coaching?
+                </h3>
+            </dt>
+            <dd className="mb-16">
+                <p>
+                Life coaching is a process of goals set by you. We all know each step has roadblocks. We discuss roadblocks and what options you have when they occur. Life coaching is a method to practice small goals towards that final goal.
+                </p>
+            </dd>
+            <dt className="mb-4">
+                <h3 className="text-xl font-semibold">
+                What is NOT Life Coaching?
+                </h3>
+            </dt>
+            <dd className="mb-16">
+            <p>Life coaching is not counseling, that's for a licensed professional. If you are having mental health issues or immediate crisis consult a licensed therapist or call 911.</p>
+
+                
+            </dd>
+            <dt className="mb-4">
+                <h3 className="text-xl font-semibold">
+                How does life coaching work?
+                </h3>
+            </dt>
+            <dd className="mb-16">
+            <p>We decide together on the best plan based on the results you are looking to accomplish. Hourly rates allow you to detemine how many visits you think you will need. Monthly rates include weekly meetings by phone, and email correspondence and require a three month commitment. 
 Life coaching is empowering and helps establish life long habits. When personal goals become a challenge to meet life coaching can help simplyfiy! Sign up today for a free assessment!</p>
+
+            </dd>
+        </dl>
     </div>
+</div>
+
+    
   )
 }
 

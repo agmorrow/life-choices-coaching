62aaef7963189c4b58f64ac655cecab76faee0a0
agmorrow <morrow.austin5@gmail.com>
2022-07-28 14:38:52 -0600

    fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,118 +1,348 @@
-import { Fragment } from 'react'
+// import { Fragment } from 'react'
+// import { Link } from 'react-router-dom';
+// import { Disclosure, Menu, Transition } from '@headlessui/react'
+// import { MenuIcon, XIcon } from '@heroicons/react/outline'
+// import Button from './Button';
+// import LifeChoicesLogo from '../assets/life_choices.png';
+
+// const navigation = [
+//   { name: 'Home', href: '/' },
+//   { name: 'Services', href: '/services' },
+//   { name: 'FAQ', href: '/faq' },
+//   { name: 'About', href: '/about' },
+// ]
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
+// export default function Example() {
+//   return (
+//     <Disclosure as="nav">
+//       {({ open }) => (
+//         <>
+//           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+//             <div className="relative flex items-center justify-between h-20">
+//               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+//                 {/* Mobile menu button*/}
+//                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+//                   <span className="sr-only">Open main menu</span>
+//                   {open ? (
+//                     <XIcon className="block h-6 w-6" aria-hidden="true" />
+//                   ) : (
+//                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+//                   )}
+//                 </Disclosure.Button>
+//               </div>
+//               <div className="flex-1 flex items-center justify-center sm:items-center sm:justify-start">
+//                 <div className="flex-shrink-0 flex items-center">
+//                   <img
+//                     className="block lg:hidden h-32 mt-12 w-auto"
+//                     src={LifeChoicesLogo}
+//                     alt="Workflow"
+//                   />
+//                   <img
+//                     className="hidden lg:block h-36 mt-20 w-auto"
+//                     src={LifeChoicesLogo}
+//                     alt="Workflow"
+//                   />
+               
+//                 </div>
+//                 <div className="hidden mt-24 sm:flex sm:ml-96">
+//                   <div className="flex space-x-4">
+//                     {navigation.map((item) => (
+//                       <a
+//                         key={item.name}
+//                         href={item.href}
+//                         className={classNames(
+//                           item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
+//                           'px-3 py-2 rounded-md text-sm font-medium'
+//                         )}
+//                         aria-current={item.current ? 'page' : undefined}
+//                       >
+//                         {item.name}
+//                       </a>
+//                     ))}
+//                     <Link as={Link} to='/contact' className="mt-1.5"><Button>Contact</Button></Link>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                
+
+//                 {/* Profile dropdown */}
+//                 <Menu as="div" className="ml-3 relative">
+              
+//                   <Transition
+//                     as={Fragment}
+//                     enter="transition ease-out duration-100"
+//                     enterFrom="transform opacity-0 scale-95"
+//                     enterTo="transform opacity-100 scale-100"
+//                     leave="transition ease-in duration-75"
+//                     leaveFrom="transform opacity-100 scale-100"
+//                     leaveTo="transform opacity-0 scale-95"
+//                   >
+//                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                     
+//                     </Menu.Items>
+//                   </Transition>
+//                 </Menu>
+//               </div>
+//             </div>
+//           </div>
+
+//           <Disclosure.Panel className="sm:hidden">
+//             <div className="px-2 pt-2 pb-3 space-y-1">
+//               {navigation.map((item) => (
+//                 <Disclosure.Button
+//                   key={item.name}
+//                   as="a"
+//                   href={item.href}
+//                   className={classNames(
+//                     item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
+//                     'block px-3 py-2 rounded-md text-base font-medium'
+//                   )}
+//                   aria-current={item.current ? 'page' : undefined}
+//                   >
+                 
+//                   {item.name}
+//                 </Disclosure.Button>
+//               ))}
+//             </div>
+//           </Disclosure.Panel>
+//         </>
+//       )}
+//     </Disclosure>
+//   )
+// }
+
 import { Link } from 'react-router-dom';
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Button from './Button';
 import LifeChoicesLogo from '../assets/life_choices.png';
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+  BookmarkAltIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  CursorClickIcon,
+  MenuIcon,
+  RefreshIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  ViewGridIcon,
+  XIcon,
+} from '@heroicons/react/outline'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
-const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'Services', href: '/services' },
-  { name: 'FAQ', href: '/faq' },
-  { name: 'About', href: '/about' },
+const services = [
+  {
+    name: 'Christian Counseling',
+    description: 'Get a better understanding of where your traffic is coming from.',
+    href: '#',
+    icon: ChartBarIcon,
+  },
+  {
+    name: 'Crisis Coaching',
+    description: 'Speak directly to your customers in a more meaningful way.',
+    href: '#',
+    icon: CursorClickIcon,
+  },
+  {
+    name: 'Self Care',
+    description: "Connect with third-party tools that you're already using.",
+    href: '#',
+    icon: ViewGridIcon,
+  },
+  {
+    name: 'Work / Life Balance',
+    description: 'Build strategic funnels that will drive your customers to convert',
+    href: '#',
+    icon: RefreshIcon,
+  },
+]
+
+const resources = [
+  {
+    name: 'Home',
+    description: 'Get all of your questions answered in our forums or contact support.',
+    href: '#',
+    icon: SupportIcon,
+  },
+  {
+    name: 'FAQ',
+    description: 'Learn how to maximize our platform to get the most out of it.',
+    href: '#',
+    icon: BookmarkAltIcon,
+  },
+  {
+    name: 'About',
+    description: 'See what meet-ups and other events we might be planning near you.',
+    href: '#',
+    icon: CalendarIcon,
+  },
+  { name: 'Contact',
+    description: 'Understand how we take your privacy seriously.',
+    href: '#', 
+    icon: ShieldCheckIcon },
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Navbar() {
   return (
-    <Disclosure as="nav">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-20">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-32 mt-12 w-auto"
-                    src={LifeChoicesLogo}
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-36 mt-20 w-auto"
-                    src={LifeChoicesLogo}
-                    alt="Workflow"
-                  />
-               
-                </div>
-                <div className="hidden mt-32 sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                    <Link as={Link} to='/contact' className="mt-1.5"><Button>Contact</Button></Link>
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                
+    <Popover className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src={LifeChoicesLogo}
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden md:flex space-x-10">
+          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Home
+            </a>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                    )}
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-              
+                  >
+                    <span>Services</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
                   <Transition
                     as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                     
-                    </Menu.Items>
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {services.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                            >
+                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                              <div className="ml-4">
+                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                              </div>
+                            </a>
+                          ))}
+                        </div>
+                       
+                      </div>
+                    </Popover.Panel>
                   </Transition>
-                </Menu>
+                </>
+              )}
+            </Popover>
+
+           
+            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              FAQ
+            </a>
+            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              About
+            </a>
+            <Link as={Link} to='/contact'><Button>Contact</Button></Link>
+          </Popover.Group>
+      
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src={LifeChoicesLogo}
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {services.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                    >
+                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                    </a>
+                  ))}
+                </nav>
               </div>
             </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
+            <div className="py-6 px-5 space-y-6">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+             
+                {resources.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                 
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+          
             </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
-

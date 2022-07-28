5e3a8e3ee11768d6b4305948dd08bf3d43c30e3b
agmorrow <morrow.austin5@gmail.com>
2022-07-28 17:56:03 -0600

    started contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
-const Contact = () => {
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+export default function Example() {
   return (
-    <div className='bg-green-600 w-full h-screen'>Contact</div>
+    <>
+      <div>
+        <h1>Contact</h1>
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form action="#" method="POST">
+              <div className="shadow sm:rounded-md sm:overflow-hidden">
+                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                        Subject
+                      </label>
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  <div>
+                    <label htmlFor="about" className="block text-sm font-medium text-gray-700">
+                      About
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        placeholder="you@example.com"
+                        defaultValue={''}
+                      />
+                    </div>
+                  
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+
+   
+    </>
   )
 }
-
-export default Contact
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,121 +1,3 @@
-// import { Fragment } from 'react'
-// import { Link } from 'react-router-dom';
-// import { Disclosure, Menu, Transition } from '@headlessui/react'
-// import { MenuIcon, XIcon } from '@heroicons/react/outline'
-// import Button from './Button';
-// import LifeChoicesLogo from '../assets/life_choices.png';
-
-// const navigation = [
-//   { name: 'Home', href: '/' },
-//   { name: 'Services', href: '/services' },
-//   { name: 'FAQ', href: '/faq' },
-//   { name: 'About', href: '/about' },
-// ]
-
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(' ')
-// }
-
-// export default function Example() {
-//   return (
-//     <Disclosure as="nav">
-//       {({ open }) => (
-//         <>
-//           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-//             <div className="relative flex items-center justify-between h-20">
-//               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-//                 {/* Mobile menu button*/}
-//                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-//                   <span className="sr-only">Open main menu</span>
-//                   {open ? (
-//                     <XIcon className="block h-6 w-6" aria-hidden="true" />
-//                   ) : (
-//                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-//                   )}
-//                 </Disclosure.Button>
-//               </div>
-//               <div className="flex-1 flex items-center justify-center sm:items-center sm:justify-start">
-//                 <div className="flex-shrink-0 flex items-center">
-//                   <img
-//                     className="block lg:hidden h-32 mt-12 w-auto"
-//                     src={LifeChoicesLogo}
-//                     alt="Workflow"
-//                   />
-//                   <img
-//                     className="hidden lg:block h-36 mt-20 w-auto"
-//                     src={LifeChoicesLogo}
-//                     alt="Workflow"
-//                   />
-               
-//                 </div>
-//                 <div className="hidden mt-24 sm:flex sm:ml-96">
-//                   <div className="flex space-x-4">
-//                     {navigation.map((item) => (
-//                       <a
-//                         key={item.name}
-//                         href={item.href}
-//                         className={classNames(
-//                           item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
-//                           'px-3 py-2 rounded-md text-sm font-medium'
-//                         )}
-//                         aria-current={item.current ? 'page' : undefined}
-//                       >
-//                         {item.name}
-//                       </a>
-//                     ))}
-//                     <Link as={Link} to='/contact' className="mt-1.5"><Button>Contact</Button></Link>
-//                   </div>
-//                 </div>
-//               </div>
-//               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                
-
-//                 {/* Profile dropdown */}
-//                 <Menu as="div" className="ml-3 relative">
-              
-//                   <Transition
-//                     as={Fragment}
-//                     enter="transition ease-out duration-100"
-//                     enterFrom="transform opacity-0 scale-95"
-//                     enterTo="transform opacity-100 scale-100"
-//                     leave="transition ease-in duration-75"
-//                     leaveFrom="transform opacity-100 scale-100"
-//                     leaveTo="transform opacity-0 scale-95"
-//                   >
-//                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                     
-//                     </Menu.Items>
-//                   </Transition>
-//                 </Menu>
-//               </div>
-//             </div>
-//           </div>
-
-//           <Disclosure.Panel className="sm:hidden">
-//             <div className="px-2 pt-2 pb-3 space-y-1">
-//               {navigation.map((item) => (
-//                 <Disclosure.Button
-//                   key={item.name}
-//                   as="a"
-//                   href={item.href}
-//                   className={classNames(
-//                     item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
-//                     'block px-3 py-2 rounded-md text-base font-medium'
-//                   )}
-//                   aria-current={item.current ? 'page' : undefined}
-//                   >
-                 
-//                   {item.name}
-//                 </Disclosure.Button>
-//               ))}
-//             </div>
-//           </Disclosure.Panel>
-//         </>
-//       )}
-//     </Disclosure>
-//   )
-// }
-
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import LifeChoicesLogo from '../assets/life_choices.png';
@@ -215,6 +97,9 @@ export default function Navbar() {
           <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Home
             </a>
+            <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              About
+            </a>
             <Popover className="relative">
               {({ open }) => (
                 <>
@@ -272,9 +157,6 @@ export default function Navbar() {
            
             <a href="/faq" className="text-base font-medium text-gray-500 hover:text-gray-900">
               FAQ
-            </a>
-            <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              About
             </a>
             <Link as={Link} to='/contact'><Button>Contact</Button></Link>
           </Popover.Group>

a832349639c33f6f8211d58e36094d08870ebc41
Austin Morrow <morrow.austin5@gmail.com>
2022-11-21 23:10:47 -0700

    Deleted Services tab

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'; 
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import ChristianCounseling from './pages/dropdown/ChristianCounseling';
-import CrisisCoaching from './pages/dropdown/CrisisCoaching';
-import SelfCare from './pages/dropdown/SelfCare.jsx';
-import WorkLife from './pages/dropdown/WorkLife';
 import FAQ from './pages/FAQ';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -19,10 +15,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/christiancounseling' element={<ChristianCounseling/>}/>
-          <Route path='/crisiscoaching' element={<CrisisCoaching/>}/>
-          <Route path='/selfcare' element={<SelfCare/>}/>
-          <Route path='/worklife' element={<WorkLife/>}/>
           <Route path='/faq' element={<FAQ/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,70 +4,11 @@ import LifeChoicesLogo from '../assets/life_choices.png';
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import {
-  BookmarkAltIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  CursorClickIcon,
   MenuIcon,
-  RefreshIcon,
-  ShieldCheckIcon,
-  SupportIcon,
-  ViewGridIcon,
   XIcon,
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
-const services = [
-  {
-    name: 'Christian Counseling',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '/christiancounseling',
-    icon: ChartBarIcon,
-  },
-  {
-    name: 'Crisis Coaching',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: 'crisiscoaching',
-    icon: CursorClickIcon,
-  },
-  {
-    name: 'Self Care',
-    description: "Connect with third-party tools that you're already using.",
-    href: '/selfcare',
-    icon: ViewGridIcon,
-  },
-  {
-    name: 'Work / Life Balance',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: 'worklife',
-    icon: RefreshIcon,
-  },
-]
-
-const resources = [
-  {
-    name: 'Home',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '/',
-    icon: SupportIcon,
-  },
-  {
-    name: 'FAQ',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '/faq',
-    icon: BookmarkAltIcon,
-  },
-  {
-    name: 'About',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '/about',
-    icon: CalendarIcon,
-  },
-  { name: 'Contact',
-    description: 'Understand how we take your privacy seriously.',
-    href: '/contact', 
-    icon: ShieldCheckIcon },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -100,61 +41,7 @@ export default function Navbar() {
             <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
               About
             </a>
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    )}
 
-                  >
-                    <span>Services</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {services.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                            >
-                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                              </div>
-                            </a>
-                          ))}
-                        </div>
-                       
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-
-           
             <a href="/faq" className="text-base font-medium text-gray-500 hover:text-gray-900">
               FAQ
             </a>
@@ -193,34 +80,11 @@ export default function Navbar() {
               </div>
               <div className="mt-6">
                 <nav className="grid gap-y-8">
-                  {services.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                    >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                    </a>
-                  ))}
+
                 </nav>
               </div>
             </div>
-            <div className="py-6 px-5 space-y-6">
-              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-             
-                {resources.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-          
-            </div>
+
           </div>
         </Popover.Panel>
       </Transition>
